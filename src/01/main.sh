5704500e2ed0975780144254706e72e5dda81d48
Massappeals <massappeals@mail.ru>
2023-06-26 02:45:09 +0700

    edit 01

--- src/01/main.sh
+++ src/01/main.sh
@@ -1,10 +1,56 @@
 #!/bin/bash
 
-#main.sh /opt/test 4 az 5 az.az 3kb
+# main.sh /opt/test 4 az 5 az.az 3kb
+# echo $1, $2, $3, $4, $5, $6
+path=$1
+c_dir=$2
+name_dir=$3
+c_file=$4
+name_file=$5
+size=$6
+
+function check_directory {
+    if ! [ -d "$1" ]; then
+        echo "Директории не существует!"
+    else
+        check_parametr
+    fi
+}
+
+function check_parametr {
+  if [ -z "$c_dir" ] || ! [ "$c_dir" =~ ^[1-9]$|^10$ ]
+  then
+    echo "Второй параметр должен быть числом!"
+    exit 1
+  fi
+
+  if [ -z "$name_dir" ] || [ ! "$name_dir" =~ ^[a-z]{1,7}$ ]
+  then
+    echo "Третий параметр должен содержать не более 7 латинских символов!"
+    exit 1
+  fi
+
+  if [ -z "$c_file" ] || ! [ "$c_file" =~ ^[1-9]$|^10[0-9]$|^100$ ]
+  then
+    echo "Четвертый параметр должен быть числом от 1 до 100!"
+    exit 1
+  fi
 
 
+  if [ -z "$name_file" ] || [ ! "$name_file" =~ ^[a-z]{1,7}\.[a-z]{1,3}$ ]
+  then
+    echo "Пятый параметр должен содержать имя файла и расширение, не более 7 и 3 латинских символов соответственно!"
+    exit 1
+  fi
 
-. ./error_dir.sh
+  if [ -z "$size" ] || ! [ "$size" =~ ^[1-9]$|^[1-9][0-9]$|^100$ ]
+  then
+    echo "Шестой параметр должен быть числом от 1 до 100!"
+    exit 1
+  fi
+}
+
+check_directory
 . ./system.sh
 
 exit 0
--- src/01/system.sh
+++ src/01/system.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# main.sh /opt/test 4 az 5 az.az 3kb
+# echo $1, $2, $3, $4, $5, $6
 path=$1
 c_dir=$2
 name_dir=$3
@@ -7,37 +9,48 @@ c_file=$4
 name_file=$5
 size=$6
 
-cd $path
-
-for (( i=1; i<=$c_dir; i++ ))
-do
-  folder_name="${name_dir:0:$i}"
-  folder_path="$path/$folder_name"
-  mkdir "$folder_path"
-  echo "$(date "+%d.%m.%y %T"): create dir - $folder_path" >> script.log
-done
-
-random=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c $((size * 1000)))
-for folder in "$1"/*
-do
-  for (( i=1; i<=c_file; i++ ))
-  do
-    file_n="${name_dir:0:$((i % c_dir))}${name_file}"
-    while [[ "$file_n" =~ az ]]
-    do
-      file_n=$(echo "$file_n" | sed "s/az/za/")
-    done
-    file_path="$folder/$file_n"
-    echo "$random" > "$file_path"
-    echo "$(date "+%d.%m.%y %T"): create file - $file_path" >> script.log
-    # Проверяем не осталось ли менее 1Гб свободного места на диске
-    free_space=$(df -B 1G / | awk '{print $4}' | tail -n +2)
-    if (( free_space < 1 )); then
-      echo "На диске осталось менее 1Гб свободного места!"
-      exit 1
+function check_directory {
+    if ! [ -d "$1" ]; then
+        echo "Директории не существует!"
+    else
+        check_parametr
     fi
-  done
-done
+}
 
-echo "Скрипт завершен" >> script.log
+function check_parametr {
+  if [ -z "$c_dir" ] || ! [ "$c_dir" =~ ^[1-9]$|^10$ ]
+  then
+    echo "Второй параметр должен быть числом!"
+    exit 1
+  fi
+
+  if [ -z "$name_dir" ] || [ ! "$name_dir" =~ ^[a-z]{1,7}$ ]
+  then
+    echo "Третий параметр должен содержать не более 7 латинских символов!"
+    exit 1
+  fi
+
+  if [ -z "$c_file" ] || ! [ "$c_file" =~ ^[1-9]$|^10[0-9]$|^100$ ]
+  then
+    echo "Четвертый параметр должен быть числом от 1 до 100!"
+    exit 1
+  fi
+
+
+  if [ -z "$name_file" ] || [ ! "$name_file" =~ ^[a-z]{1,7}\.[a-z]{1,3}$ ]
+  then
+    echo "Пятый параметр должен содержать имя файла и расширение, не более 7 и 3 латинских символов соответственно!"
+    exit 1
+  fi
+
+  if [ -z "$size" ] || ! [ "$size" =~ ^[1-9]$|^[1-9][0-9]$|^100$ ]
+  then
+    echo "Шестой параметр должен быть числом от 1 до 100!"
+    exit 1
+  fi
+}
+
+check_directory
+. ./system.sh
+
 exit 0
