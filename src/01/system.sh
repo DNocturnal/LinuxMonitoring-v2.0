3014bedcbe9c397b9444ddff38e608c25cd3dbb5
Massappeals <massappeals@mail.ru>
2023-06-27 03:10:42 +0700

    system

--- src/01/system.sh
+++ src/01/system.sh
@@ -1,56 +1,39 @@
-#!/bin/bash
-
-# main.sh /opt/test 4 az 5 az.az 3kb
-# echo $1, $2, $3, $4, $5, $6
 path=$1
 c_dir=$2
 name_dir=$3
 c_file=$4
 name_file=$5
 size=$6
+file_date=$(date +"_%d%m%y")
 
-function check_directory {
-    if ! [ -d "$1" ]; then
-        echo "Директории не существует!"
-    else
-        check_parametr
+for (( i=1; i<=$c_dir; i++ ))
+do
+  folder_name="${name_dir:0:$i}"
+  folder_path="$path/$folder_name"
+  mkdir "$folder_path$file_date"
+  echo "$file_date: create dir - $folder_path" >> script.log
+done
+
+random=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c $((size * 1024)))
+for folder in "$1"/*
+do
+  for (( i=1; i<=c_file; i++ ))
+  do
+    file_n="${name_dir:0:$((i % c_dir))}${name_file}"
+    while [[ "$file_n" =~ az ]]
+    do
+      file_n=$(echo "$file_n" | sed "s/az/za/")
+    done
+    file_path="$folder/$file_n"
+    echo "$random" > "$file_path"
+    echo "$(date "+%d.%m.%y %T"): create file - $file_path" >> script.log
+    free_space=$(df -B 1G / | awk '{print $4}' | tail -n +2)
+    if (( free_space < 1 )); then
+      echo -e "\033[41m На диске осталось менее 1Гб свободного места!"
+      exit 1
     fi
-}
+  done
+done
 
-function check_parametr {
-  if [ -z "$c_dir" ] || ! [ "$c_dir" =~ ^[1-9]$|^10$ ]
-  then
-    echo "Второй параметр должен быть числом!"
-    exit 1
-  fi
-
-  if [ -z "$name_dir" ] || [ ! "$name_dir" =~ ^[a-z]{1,7}$ ]
-  then
-    echo "Третий параметр должен содержать не более 7 латинских символов!"
-    exit 1
-  fi
-
-  if [ -z "$c_file" ] || ! [ "$c_file" =~ ^[1-9]$|^10[0-9]$|^100$ ]
-  then
-    echo "Четвертый параметр должен быть числом от 1 до 100!"
-    exit 1
-  fi
-
-
-  if [ -z "$name_file" ] || [ ! "$name_file" =~ ^[a-z]{1,7}\.[a-z]{1,3}$ ]
-  then
-    echo "Пятый параметр должен содержать имя файла и расширение, не более 7 и 3 латинских символов соответственно!"
-    exit 1
-  fi
-
-  if [ -z "$size" ] || ! [ "$size" =~ ^[1-9]$|^[1-9][0-9]$|^100$ ]
-  then
-    echo "Шестой параметр должен быть числом от 1 до 100!"
-    exit 1
-  fi
-}
-
-check_directory
-. ./system.sh
-
+echo "Скрипт завершен" >> script.log
 exit 0
