86472cb4b6f9460eba57643e60e19541123c70c7
Massappeals <massappeals@mail.ru>
2023-06-27 18:36:42 +0700

    50%

--- src/05/main.sh
+++ src/05/main.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 
-# Скрипт запускается с 1 параметром, который принимает значение 1, 2, 3 или 4. В зависимости от значения параметра вывести:
-
-# Все записи, отсортированные по коду ответа
 # Все уникальные IP, встречающиеся в записях
-# Все запросы с ошибками (код ответа - 4хх или 5хх)
+
 # Все уникальные IP, которые встречаются среди ошибочных запросов
 
+./../04/main.sh
+
 request_code() {
-
+    for i in {1..5}
+    do
+        awk '{print $0}' access_log$i.log | sort -k9 >> Request_Code_$i.log
+    done 
 }
-uniq_ip() {
 
-}
 select_error() {
-
+    for i in {1..5}
+    do
+        awk '$9 ~ /^4/ || $9 ~ /^5/ {print $0}' access_log$i.log >> Select_Error_$i.log
+    done 
 }
-uniq_ip_error() {
 
-} 
+uniq_ip() {
+    for i in {1..5}
+    do
+        awk '{print $1}' access_log$i.log >> Unique_IP_$i.log
+    done 
+}
+uniq_ip
+# uniq_ip_error() {
 
-if [ $1 -eq 1]; then
-    request_code
-elif [ $1 -eq 2 ]; then 
-    uniq_ip
-elif [ $1 -eq 3 ]; then 
-    select_error
-elif [ $1 -eq 4 ]; then 
-    uniq_ip_error
-else
-    echo "Error $1 parametr! Select 1, 2, 3 or 4"
-fi
+# } 
+
+# if [ $1 -eq 1]; then
+#     request_code
+# elif [ $1 -eq 2 ]; then 
+#     uniq_ip
+# elif [ $1 -eq 3 ]; then 
+#     select_error
+# elif [ $1 -eq 4 ]; then 
+#     uniq_ip_error
+# else
+#     echo "Error $1 parametr! Select 1, 2, 3 or 4"
+# fi
